51c33a33cf3de2b8bff52262ff67756566a048d2
Andrei Tsapliuk <webbridge@mail.ru>
2019-05-04 13:12:19 +0300

    Added button for handling function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
+  handleFunction = () => {
+    fetch('https://upbeat-euler-c4c81e.netlify.com/.netlify/functions/hello').then(res => {
+      console.log(res)
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,17 +16,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
+        <button onClick={this.handleFunction}>lambda function</button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
